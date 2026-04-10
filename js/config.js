45fd5e6e5be1e90b76eaf71969ad47a0ef81200d
basemelsayed540 <basem2elsayed@gmail.com>
2026-04-10 12:58:00 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    SUPABASE_URL: "https://gmrytacdqydzdwhwcgqo.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtcnl0YWNkcXlkemR3aHdjZ3FvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTEwNTAzNSwiZXhwIjoyMDkwNjgxMDM1fQ._LkZWnz5tC8ldDoLw6f3gqWTiIC8vOUQQO24SQAHf7E",
+    TABLES: {
+        SHIPMENTS: 'shipments1',
+        USERS: 'users'
+    }
+};
